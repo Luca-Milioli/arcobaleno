bcab28dbed4842948e08f95f3ec34c95f17fea3d
FedericoRigolon <318199@studenti.unimore.it>
2025-08-27 12:39:51 +0200

    FeedbackTimer reduced

--- arcobaleno/scripts/main.gd
+++ arcobaleno/scripts/main.gd
@@ -1,6 +1,8 @@
 ## Main. It manages transition from a scene to another (menu, gui).
 extends Node
 
+class_name Main
+
 ## URL of site that it'll be redirected to.
 const URL = "https://spreafico.net"
 
